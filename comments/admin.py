660a71142fabbf308825a090e31d2ba41e6f0cac
Andrew <andrey.karyamin@phystech.edu>
2017-04-01 17:01:10 +0300

    Migrated Blogs, Posts, Comments. Added them to admin.py. Created superuser avk.

--- comments/admin.py
+++ comments/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
 
+from blogs.models import Blog
+from comments.models import Comment
+from posts.models import Post
 
+admin.site.register(Blog)
+admin.site.register(Post)
+admin.site.register(Comment)
